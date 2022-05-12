5b3c55faadef3b85a81dc56bda5131e647e1ba9e
Suhas183 <herlesuhas@gmail.com>
2022-05-12 20:13:58 +0530

    Modified file

--- 04_2D_arrays/pascal_triangle.cpp
+++ 04_2D_arrays/pascal_triangle.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -7,34 +7,27 @@ int main()
     int n;
     cin>>n;
 
-    vector<vector<int>> v;
-
     for(int i = 0; i < n; i++)
     {
-        int count = 0;
-        v.push_back(vector<int>());
-
-        for(int j = 0; j < n + i; j++)
+        for(int s = 0; s < n - i - 1; s++)
         {
-            if(n - i - j - 1 == 0 or j == n + i - 1)
-            {
-                v[i].push_back(1);
-                cout<<"1";
-            }
+            cout<<" ";
+        }
 
-            else if(n - i - j - 1 < 0 && (n - i - j - 1) % 2 == 0)
-            {
-                v[i].push_back(v[i-1][count] + v[i-1][count + 1]);
-                cout<<(v[i-1][count] + v[i-1][count + 1]);
-                count++;
-            }
+        int temp = 1;
+        int num = i;
+        int den = 1;
 
-            else
-            {
-                cout<<" ";
-            }
+        for(int j = 0; j < i + 1; j++)
+        {
+            cout<<temp<<" ";
+            temp = temp*num;
+            temp = temp/den;
+            num--;
+            den++;
         }
 
         cout<<endl;
     }
+    return 0;
 }
